c07308eb0d41750e930eacedea4cfb3081841b44
marty <martymagaan@gmail.com>
2018-08-21 18:00:16 -0400

    Save content to file on user disconnect

--- server/src/notepub.js
+++ server/src/notepub.js
@@ -1,17 +1,31 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const fs = require('fs');
 
-let content = '';
+let content;
+
+fs.readFile('content.txt', 'utf-8', (err, data) => {
+  if (err) throw err;
+  content = data;
+});
 
 io.on('connection', (socket) => {
-  console.log('User connected');
+  console.log('User connected ' + Date().toLocaleString());
 
   socket.emit('load-content', content);
 
   socket.on('change-content', (updatedContent) => {
     content = updatedContent;
     io.emit('load-content', content);
+  });
+
+  socket.on('disconnect', () => {
+    const fileName = 'content.txt';
+    fs.writeFile(fileName, content, (err) => {
+      if (err) throw err;
+      console.log('User disconnected ' + Date().toLocaleString());
+    });
   });
 });
 
